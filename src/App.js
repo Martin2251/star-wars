1044cabad9011287cff8687f0f0b5b553e43e08b
martin lowe <martinlowe673@gmail.com>
2021-04-25 10:43:52 +0100

    first route

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 //react router https://reactrouter.com/web/guides/quick-start
+//react route video https://www.youtube.com/watch?v=Law7wfdg_ls
 
 function App() {
   return (
     <div className="App">
       <Router>
+        <Route path="./components/card" component={card}></Route>
         <div></div>
       </Router>
     </div>
