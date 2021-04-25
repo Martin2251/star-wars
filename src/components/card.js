18587ec75ed3fecb493128b2242e3063391655a3
martin lowe <martinlowe673@gmail.com>
2021-04-25 10:58:09 +0100

    added people api

--- src/components/card.js
+++ src/components/card.js
@@ -1,7 +1,16 @@
-import { useParams } from "react-router";
+import { useParams } from "react-router-dom";
+import React, { useState, useEffect } from 'react'
 
 const Card = () => {
   const [card, setCard] = useState({});
   const [filmsTitle, setFilmsTitle] = useState([])
 
   const { id } = useParams();
+
+  const fetchItems = async () => {
+    const data = await fetch(`https://swapi.dev/api/people/${id}`)
+    .then(response => {
+
+    })
+
+  }
