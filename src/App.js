40c5d0f67f558c0b6db31d282460c03dde3b8ead
martin lowe <martinlowe673@gmail.com>
2021-04-27 07:40:39 +0100

    added useEffect

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import logo from "./logo.svg";
 import "./App.css";
+import React { useState, useEffect} from 'react'
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 //react router https://reactrouter.com/web/guides/quick-start
 //react route video https://www.youtube.com/watch?v=Law7wfdg_ls
 
 function App() {
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    async function fetch ()
+  })
+
+
+
   return (
     <div className="App">
       <Router>
