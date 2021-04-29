9693dac3a7fa1842a219a38f5a7afddb5897636a
martin lowe <martinlowe673@gmail.com>
2021-04-29 08:03:21 +0100

    added async on first page

--- src/App.js
+++ src/App.js
@@ -7,10 +7,17 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 https://www.youtube.com/watch?v=aZGzwEjZrXc
 
 function App() {
-  const [loading, setLoading] = useState(true)
+  const [loading, setLoading] = useState(true);
   const
 
+useEffect(() => {
+  async function name(params) {
 
+  }
+
+
+  }
+}, [)
 
 
 
@@ -18,8 +25,9 @@ function App() {
     <div className="App">
       <Router>
         <Switch>
-          <Route path="./components/card" exact component={card}></Route></Switch>
+          <Route path="./components/card" exact component={card}></Route>
           <Route exact path='/'></Route>
+          </Switch>
         <div></div>
       </Router>
     </div>
