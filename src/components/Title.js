33d873f6ccbe679f7e3edbdcf9d0ff956c57ac2e
martin lowe <martinlowe673@gmail.com>
2021-05-03 14:09:33 +0100

    added title

---
+++ src/components/Title.js
@@ -0,0 +1,8 @@
+import { useEffect, useState } from "react";
+
+//empty state from the user
+const Title = (props) => {
+  const [titleText, setTitleText] = useState("");
+};
+
+export default Title;
