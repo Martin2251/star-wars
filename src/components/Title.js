2fbaf00bf80837e75abc336d2ed98f01dc630c84
martin lowe <martinlowe673@gmail.com>
2021-05-04 08:12:45 +0100

    added info on title page

--- src/components/Title.js
+++ src/components/Title.js
@@ -5,4 +5,12 @@ const Title = (props) => {
   const [titleText, setTitleText] = useState("");
 };
 
+return (
+  <div className="mainCard">
+    <ul className="movieBox">
+      <li>{prop}</li>
+    </ul>
+  </div>
+);
+
 export default Title;
--- src/components/card.js
+++ src/components/card.js
@@ -1,27 +1,40 @@
+import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-import { useState, useEffect } from 'react'
-import {link} from 'react-router-dom';
-import './Catalog.css'
+import { Link } from "react-router-dom";
+import Title from "./Title";
 
 const Card = () => {
   const [card, setCard] = useState({});
-  const [filmsTitle, setFilmsTitle] = useState([])
 
   const { id } = useParams();
 
-// show a person on loading first time
+  // Load the api for people
   useEffect(() => {
     fetch(`https://swapi.dev/api/people/${id}`)
-        .then(response => {
-            console.log(response)
-            // console.log(response.json())
-            return response.json()
+      .then((response) => {
+        console.log(response);
+        // console.log(response.json())
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data);
+        setCard(data);
+      });
+  }, card);
 
-        })
-        .then(data => {
-          console.log(data)
-          setCard(data)
-        })
-      }, card)
+  console.log(card);
 
-      //console.log(card)
+  //show the card and everthing else
+  return (
+    <div className="mainCard">
+      <p className="name" data-cy="card-name">
+        {card.name}
+      </p>
+      <div className="charactersFeatures">
+        <p className="charactersData">Height:{card.height} cm</p>
+        <p className="charactersData">Eye Color: {card.eye_color}</p>
+        <p className="charactersData">Hair Color: {card.hair_color}</p>
+        <p className="charactersData">Birth Date: {card.birth_year}</p>
+      </div>
+
+
