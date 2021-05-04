d43c530b326baea7cb2e16d496c0e83850c031db
martin lowe <martinlowe673@gmail.com>
2021-05-04 08:37:13 +0100

    added props on title js

--- src/components/Title.js
+++ src/components/Title.js
@@ -3,7 +3,19 @@ import { useEffect, useState } from "react";
 //empty state from the user
 const Title = (props) => {
   const [titleText, setTitleText] = useState("");
-};
+
+  console.log(props.filmLink);
+
+  useEffect(() => {
+
+    fetch(`${props.filmLink}`)
+    .then(response => response.json())
+    .then(data => {
+      setTitleText(data.title)
+    })
+}, [])
+
+// re rendering and adding props to use
 
 return (
   <div className="mainCard">
